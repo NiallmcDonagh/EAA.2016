f1ce279f7d2398c71b063b4a47f0933d905931a7
John Burns <jzburns@gmail.com>
2016-10-06 10:54:21 +0100

    comment

---
+++ week2/Lab2.sh
@@ -0,0 +1,42 @@
+
+
+echo "you entered $# parameters:"
+echo "$1 $2 $3"
+echo "is this correct"
+
+
+
+
+
+--echo $1 > myfile.txt
+
+--# capture the user input and grab it to a file:
+
+--echo $1 $2 $3 > audit.txt
+
+--echo "$1 $2 $3" >> $HOME/documents/audit.txt
+--echo $user
+--more Documents/audit.txt
+
+
+--echo $1 > myfile.txt
+
+--# capture the user input and grab it to a file:
+
+--echo $1 $2 $3 > audit.txt
+
+--echo "$1 $2 $3" >> $HOME/documents/audit.txt
+--echo " $user $1 $2 $3"  >> $HOME/documents/audit.txt 
+--echo " $(date) $USER $1 $2 $3" 
+
+--You can place anything in the file i.e file has no limits
+		-- Where writing to the file
+		Wirting to the file has only 3 parameters created
+
+--more Documents/audit.txt
+
+-- man date
+
+
+--# `command` run command as part of the echo:
+
---
+++ week2/week2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#--echo "you entered $# parameters:"
+#--echo "$1 $2 $3"
+#--echo "is this correct?"
+
+mkdir $HOME/$1backup_`date +%F - $H:$M:$S`
+
+
+
+
