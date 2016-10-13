83f9896be8cb0079d962f100e2d33202a72b0b28
John Burns <jzburns@gmail.com>
2016-10-13 10:37:55 +0100

    new file

--- week4/cntlm.sh
+++ week4/cntlm.sh
@@ -1,16 +1,16 @@
-#! bin/bash
-
-man ps -ef | grep mysql
+#! bin/bash 
 
 
-# contains mysql matched data
+ps -ef | grep cntlm
+
+# contains cntlm matched data
 
 # grep searches for a pattern in a line of text 
 
 # ps -ef prints a snapshot of every process in the system 
 
-# grep mysql searches for the text mysql
+# grep mysql searches for the text cntlm
 # where does the text come from ? - ps -ef |
 
 # This is now a new command that searches all the running process
-# looking for the mysql process 
+# looking for the cntlm process 
